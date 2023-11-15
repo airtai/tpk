dd3ce4e8b6c5d99509db93557d858d64c6176726
Sternakt <sternakt@gmail.com>
2023-11-15 15:45:07 +0100

    WIP: Implement m5 dataloader

---
+++ temporal_data_kit/datasets/m5_dataset.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+from typing import Dict
+
+import pandas as pd
+
+
+def _get_m5_dataset_raw(m5_dataset_path: Path) -> Dict[str, pd.DataFrame]:
+    dataset = {
+        "calendar": pd.read_csv(m5_dataset_path / "calendar.csv"),
+        "sales_train_validation": pd.read_csv(
+            m5_dataset_path / "sales_train_validation.csv"
+        ),
+        "sample_submission": pd.read_csv(m5_dataset_path / "sample_submission.csv"),
+        "sell_prices": pd.read_csv(m5_dataset_path / "sell_prices.csv"),
+    }
+    return dataset
+
+
+def _stack_m5_days(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.set_index(["id", "item_id", "dept_id", "cat_id", "store_id", "state_id"])
+    df = df.stack()
+    df = df.reset_index(name="n_sold")
+    df = df.rename(columns={"level_6": "day"})
+    # df["day"] = pd.to_numeric(df["day"].str.removeprefix("d_"))
+    return df
+
+
+def _combine_m5_sales_and_calendar(
+    df_sales: pd.DataFrame, df_calendar: pd.DataFrame
+) -> pd.DataFrame:
+    return pd.merge(df_sales, df_calendar, left_on="day", right_on="d")
+
+
+def _combine_m5_sales_and_sell_prices(
+    df_sales: pd.DataFrame, df_prices: pd.DataFrame
+) -> pd.DataFrame:
+    df = pd.merge(
+        df_sales, df_prices, on=["store_id", "item_id", "wm_yr_wk"], how="left"
+    )
+    df["sell_price_known"] = df["sell_price"].notna().astype(int)
+
+    mean = df.sell_price.mean(skipna=True)
+    df["sell_price"] = df["sell_price"].fillna(mean)
+
+    return df
+
+
+def get_m5_dataset(m5_dataset_path: Path) -> pd.DataFrame:
+    dataset = _get_m5_dataset_raw(m5_dataset_path)
+    sales_df = _stack_m5_days(dataset["sales_train_validation"])
+    sales_df_days = _combine_m5_sales_and_calendar(sales_df, dataset["calendar"])
+    complete_df = _combine_m5_sales_and_sell_prices(
+        sales_df_days, dataset["sell_prices"]
+    )
+    return complete_df
---
+++ tests/datasets/test_m5_dataset.py
@@ -0,0 +1,8 @@
+# id  item_id  dept_id  cat_id  store_id  state_id  d_1  d_2  d_3  d_4
+# sales_train_validation =
+
+# date  wm_yr_wk  weekday  wday  month  year  event_name_1  event_type_1  event_name_2  event_type_2  snap_CA  snap_TX  snap_WI
+# calendar =
+
+# store_id        item_id  wm_yr_wk  sell_price
+# sell_prices =
