34b6f26526d52169ac92b5620d8a502ba79fc255
Sternakt <sternakt@gmail.com>
2023-11-19 18:53:22 +0100

    Add m5 tests

--- temporal_data_kit/datasets/m5_dataset.py
+++ temporal_data_kit/datasets/m5_dataset.py
@@ -1,22 +1,8 @@
 from pathlib import Path
-from typing import Dict
+from typing import Tuple
 
 import pandas as pd
-
-
-def _get_m5_dataset_raw(m5_dataset_path: Path) -> Dict[str, pd.DataFrame]:
-    dataset = {
-        "calendar": pd.read_csv(m5_dataset_path / "calendar.csv"),
-        "sales_train_validation": pd.read_csv(
-            m5_dataset_path / "sales_train_validation.csv"
-        ),
-        "sales_train_evaluation": pd.read_csv(
-            m5_dataset_path / "sales_train_evaluation.csv"
-        ),
-        "sample_submission": pd.read_csv(m5_dataset_path / "sample_submission.csv"),
-        "sell_prices": pd.read_csv(m5_dataset_path / "sell_prices.csv"),
-    }
-    return dataset
+from torch.utils.data import Dataset
 
 
 def _stack_m5_days(df: pd.DataFrame) -> pd.DataFrame:
@@ -30,7 +16,22 @@ def _stack_m5_days(df: pd.DataFrame) -> pd.DataFrame:
 def _combine_m5_sales_and_calendar(
     df_sales: pd.DataFrame, df_calendar: pd.DataFrame
 ) -> pd.DataFrame:
-    return pd.merge(df_sales, df_calendar, left_on="day", right_on="d")
+    return pd.merge(
+        df_sales,
+        df_calendar.drop(
+            columns=[
+                "weekday",
+                "wday",
+                "month",
+                "year",
+                "snap_CA",
+                "snap_TX",
+                "snap_WI",
+            ]
+        ),
+        left_on="day",
+        right_on="d",
+    ).drop(columns=["day", "d"])
 
 
 def _combine_m5_sales_and_sell_prices(
@@ -44,22 +45,42 @@ def _combine_m5_sales_and_sell_prices(
     mean = df.sell_price.mean(skipna=True)
     df["sell_price"] = df["sell_price"].fillna(mean)
 
-    return df
+    return df.drop(columns=["wm_yr_wk"])
 
 
 def _combine_dataset(
     df_sales: pd.DataFrame, df_calendar: pd.DataFrame, df_prices: pd.DataFrame
 ) -> pd.DataFrame:
-    sales_df_days = _combine_m5_sales_and_calendar(df_sales, df_calendar)
+    sales_df_stacked = _stack_m5_days(df_sales)
+    sales_df_days = _combine_m5_sales_and_calendar(sales_df_stacked, df_calendar)
     complete_df = _combine_m5_sales_and_sell_prices(sales_df_days, df_prices)
     return complete_df
 
 
-def get_m5_dataset(m5_dataset_path: Path) -> pd.DataFrame:
-    dataset = _get_m5_dataset_raw(m5_dataset_path)
+def get_m5_dataset(m5_dataset_path: Path, test: bool = False) -> pd.DataFrame:
+    df_calendar = pd.read_csv(m5_dataset_path / "calendar.csv")
+    df_sales = pd.read_csv(
+        m5_dataset_path
+        / (("sales_train_" + "validation" if not test else "evaluation") + ".csv")
+    )
+    df_prices = pd.read_csv(m5_dataset_path / "sell_prices.csv")
 
     return _combine_dataset(
-        dataset["sales_train_validation"], dataset["calendar"], dataset["sell_prices"]
-    ), _combine_dataset(
-        dataset["sales_train_evaluation"], dataset["calendar"], dataset["sell_prices"]
+        df_sales=df_sales, df_calendar=df_calendar, df_prices=df_prices
     )
+
+
+class M5(Dataset):  # type: ignore
+    def __init__(  # type: ignore
+        self, m5_dataset_path: Path = Path("./data/m5/"), test: bool = False, **kwargs
+    ):
+        self.dataset = get_m5_dataset(
+            m5_dataset_path=m5_dataset_path, test=test, **kwargs
+        )
+
+    def __len__(self) -> int:
+        return len(self.dataset)
+
+    def __getitem__(self, idx: int) -> Tuple[pd.Series, int]:
+        sample = self.dataset.iloc[idx]
+        return sample.drop("n_sold"), sample["n_sold"]
