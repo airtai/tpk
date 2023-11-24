0d5e247b39b24628d4d12bf24bfb12f0fb4409ad
Davor Runje <davor@airt.ai>
2023-11-24 11:09:05 +0100

    data download added

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+# content of conftest.py
+
+
+from temporal_data_kit.utils.dataset import download_datasets
+
+
+def pytest_configure(config):
+    """
+    Allows plugins and conftest files to perform initial configuration.
+    This hook is called for every plugin and initial conftest
+    file after command line options have been parsed.
+    """
+    download_datasets()
+ 
+def pytest_collection_modifyitems(items):
+    for item in items:
+        item.add_marker("all")
